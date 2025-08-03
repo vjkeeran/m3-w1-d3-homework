079290ba6d3ce990a62130e2d5dc2fcb3d56647f
vjkeeran <v.keeranagi.9141@westcliff.edu>
2025-08-03 19:48:59 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Configure Pug
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
